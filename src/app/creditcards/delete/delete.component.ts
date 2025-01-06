a184fd2e53bff3e217f30c76db226eeaa3316045
Arafat <arafat.developer2008@gmail.com>
2025-01-06 10:44:05 +0600

    part 9 Compelete

--- src/app/creditcards/delete/delete.component.ts
+++ src/app/creditcards/delete/delete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CreditcardsService } from '../../services/creditcards.service';
 
 @Component({
   selector: 'app-delete',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './delete.component.css'
 })
 export class DeleteComponent {
+  creditCardId!: number;
+  constructor(private router:ActivatedRoute,
+    private route: Router,
+    private creditcardsService:CreditcardsService){
+    this.creditCardId = parseInt(this.router.snapshot.paramMap.get("id") || '');
 
+      //Delete Functionality
+      this.creditcardsService.deleteCreditCard(this.creditCardId).subscribe(data =>{
+         // console.log("Deleted this credit cards"))
+         alert("Credit Card Deleted")
+         this.route.navigate(['creditCards']);
+      })
+  }
 }
