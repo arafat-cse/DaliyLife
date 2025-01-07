9d39e95f6e44d44f0d3e18c9e37aff2ed5d0466c
Arafat <arafat.developer2008@gmail.com>
2025-01-08 01:45:24 +0600

    Part10 Compelete

--- src/app/creditcards/add/add.component.ts
+++ src/app/creditcards/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreditCard } from '../../models/credit-card';
 
 @Component({
   selector: 'app-add',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AddComponent {
 
+  newCreditCard:CreditCard={
+    id:0,
+    name: "",
+    bankName: "",
+    description: "",
+    maxCredit: 5000,
+    interestRate: 12,
+    active: true,
+    recommendedScore: "100-200",
+    annualFee: 12,
+    termsAndConditions: "terms and condition for the credit card",
+    createDate:  Date(),
+    updateDate: Date()
+
+  };
+
+
+  saveCreditCard(){
+    console.log("form Submitted");
+    console.log(this.newCreditCard);
+  }
 }
